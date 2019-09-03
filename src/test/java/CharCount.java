103bbf5406105ab08bca663a20197e6d54319689
leonidsvitols <leonidsvitols67@gmail.com>
2019-09-03 19:43:01 +0300

    Star project

--- src/test/java/CharCount.java
+++ src/test/java/CharCount.java
@@ -1,17 +1,24 @@
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class CharCount {
-    String someString = "Have you ever thought about sex?";
-    char someChar = 'e';
-    int count = 0;
 
-for (int i = 0; i < someString.length(); i++) {
-        if (someString.charAt(i) == someChar) {
-            count++;
+    @Test
+    public void someTestMethod() {
+        String someString = "Have you ever thought about sex?";
+        char someChar = 'e';
+        int count = 0;
+
+        for (int i = 0; i < someString.length(); i++) {
+            if (someString.charAt(i) == someChar) {
+                count++;
+            }
+
+            System.out.println("You had it  " + count + " times, boy.");
+            Assertions.assertEquals(4, count);
+
         }
     }
-
 }
-@Test
 
 

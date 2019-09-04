2201a1bdfb68a802753b18195f618f49a9af750a
leonidsvitols <leonidsvitols67@gmail.com>
2019-09-04 19:06:27 +0300

    Star project syntax error resolved

--- src/test/java/CharCount.java
+++ src/test/java/CharCount.java
@@ -5,19 +5,22 @@ public class CharCount {
 
     @Test
     public void someTestMethod() {
-        String someString = "Have you ever thought about sex?";
+        String someString;
+        someString = "Have you ever thought about sex, baby?";
         char someChar = 'e';
         int count = 0;
+
 
         for (int i = 0; i < someString.length(); i++) {
             if (someString.charAt(i) == someChar) {
                 count++;
             }
+        }
 
-            System.out.println("You had it  " + count + " times, boy.");
+            System.out.println("I have it  " + count + " times a day at work.");
             Assertions.assertEquals(4, count);
 
-        }
+
     }
 }
 
